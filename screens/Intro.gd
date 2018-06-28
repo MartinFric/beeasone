9cc0a79314b96e8dfc9e1d1a96de5ea1583ac399
Filip Kšenzuliak <filipksenzuliak@gmail.com>
2018-06-28 19:51:42 +0200

    add timer

--- screens/Intro.gd
+++ screens/Intro.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Node
 
 # class member variables go here, for example:
 # var a = 2
@@ -7,21 +7,13 @@ extends MarginContainer
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	set_process(true)
+	get_node("Timer").start()
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
-func _on_new_game_pressed():
-	get_tree().change_scene("res://screens/Intro.tscn")
 
-
-func _on_exit_pressed():
-	get_tree().quit()
-
-
-func _process(delta):
-   if Input.is_action_pressed("key_exit"):
-      get_tree().quit()
+func _on_Timer_timeout():
+	get_tree().change_scene("res://screens/MainMenu.tscn")
