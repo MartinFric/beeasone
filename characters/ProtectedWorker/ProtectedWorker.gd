208e88fd78ff822121a10c28365a38f9ff36e9d2
Filip Kšenzuliak <filipksenzuliak@gmail.com>
2018-06-29 04:37:36 +0200

    add workers

---
+++ characters/ProtectedWorker/ProtectedWorker.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+# class member variables go here, for example:
+signal killed
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if (body.get_name() == "Hornet"):
+		hide()
+		emit_signal("killed")
